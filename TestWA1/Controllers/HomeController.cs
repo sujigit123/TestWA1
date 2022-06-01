cb13a0534703e396310fa3be0345752147390398
SKSahoo <sujit1.dev@gmail.com>
2022-06-01 22:58:01 +0530

    aaa

--- TestWA1/Controllers/HomeController.cs
+++ TestWA1/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace TestWA1.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(); ;
         }
 
         public IActionResult Privacy()
